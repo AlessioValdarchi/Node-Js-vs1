33e6027a4b668fad01a8379ed7677dd839d9fde5
AlessioValdarchi <valdainto994@gmail.com>
2022-10-12 10:23:03 +0200

    fix es9

--- dist/appExpress.js
+++ dist/appExpress.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
+const node_fetch_1 = __importDefault(require("node-fetch"));
 const app = (0, express_1.default)();
 app.get("/serieA", (request, response) => {
     response.json([
@@ -13,5 +14,12 @@ app.get("/serieA", (request, response) => {
         },
         { name: "Inter" },
     ]);
+});
+app.get("/frasi", async () => {
+    const response = await (0, node_fetch_1.default)("http://numbersapi.com/random/math");
+    const frase = await response.text();
+    const splitfrase = frase.split(" ");
+    const splittext = splitfrase[0];
+    console.log(splittext);
 });
 exports.default = app;
--- src/appExpress.ts
+++ src/appExpress.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import "express-async-errors";
-
+import fetch from "node-fetch";
 const app = express();
 
 app.get("/serieA", (request, response) => {
@@ -10,6 +10,15 @@ app.get("/serieA", (request, response) => {
         },
         { name: "Inter" },
     ]);
+});
+
+app.get("/frasi", async () => {
+    const response = await fetch("http://numbersapi.com/random/math");
+    const frase = await response.text();
+    const splitfrase = frase.split(" ");
+    const splittext = splitfrase[0];
+
+    console.log(splittext);
 });
 
 export default app;
